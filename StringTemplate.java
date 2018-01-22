0217828f4698f9bc52c8c876e848d8bce58f140f
SAURABHWAKSHE <32311169+SAURABHWAKSHE@users.noreply.github.com>
2018-01-22 22:55:44 +0530

    Update StringTemplate.java

--- StringTemplate.java
+++ StringTemplate.java
@@ -22,9 +22,9 @@ public class StringTemplate
 		System.out.println("Enter The String:");
 		String s = sc.next();
 		if(s.length()>3)
-		System.out.println("Hello "+s+",How are you?");
+		     System.out.println("Hello "+s+",How are you?");
 		else
-			System.out.println("Username must have atleast 3 characters.");
+		     System.out.println("Username must have atleast 3 characters.");
 
 	}
 
